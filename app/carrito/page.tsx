688217b2530c9d46cc7336ca47c2f37460d5c668
JonathanLL2811 <156973028+JonathanLL2811@users.noreply.github.com>
2024-11-07 09:43:41 -0600

    Add files via upload

---
+++ app/carrito/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Carrito = () => {
+  return (
+    <div>
+      <h1>Carrito de compras</h1>
+      <p>Revisa los productos en tu carrito.</p>
+    </div>
+  );
+};
+
+export default Carrito;
+
---
+++ app/producto/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Producto = () => {
+  return (
+    <div>
+      <h1>Producto</h1>
+      <p>Detalles sobre el producto.</p>
+    </div>
+  );
+};
+
+export default Producto;
